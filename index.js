4f1c209e67983335e7f50e0569fe60352e62b6cf
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-07-02 09:40:50 -0700

    Added include statements and get mongo url and port from env file instead now.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
 
-const PORT = 4000
+const { MONGO_URL, PORT } = process.env
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
