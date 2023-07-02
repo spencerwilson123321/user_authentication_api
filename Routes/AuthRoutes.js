a618bbf57c530c747fa5bef9ea7c7ca867f5be33
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-07-02 11:00:39 -0700

    Added the verification endpoint to the route

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -1,7 +1,8 @@
-const { Signup, Login } = require("../Controllers/AuthController")
+const { Signup, Login, Verify } = require("../Controllers/AuthController")
 const router = require("express").Router()
 
 router.post("/signup", Signup)
 router.post("/login", Login)
+router.post("/verify", Verify)
 
 module.exports = router
