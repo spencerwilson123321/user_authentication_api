0727d87645b03802070dee9b6db56e865ebc0ce3
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-07-02 09:44:38 -0700

    Server uses cors middleware, json parser, and connects to the MongoDB now.

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ require("dotenv").config()
 
 const { MONGO_URL, PORT } = process.env
 
+app.use(
+    cors({
+        origin: ["http://localhost:4000"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+)
 
+app.use(express.json())
+
+mongoose.connect(
+    MONGO_URL,
+    {
+        useNewURLParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((error) => console.log(error))
+
+    
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
